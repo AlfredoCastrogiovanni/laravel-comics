a674e3fa5cf0b8200734c6d209cbfb7c63cd48ff
Alfredo Castrogiovanni <castro.alfry@gmail.com>
2024-01-31 17:06:53 +0100

    create banner

--- resources/views/pages/comics.blade.php
+++ resources/views/pages/comics.blade.php
@@ -23,4 +23,29 @@
             </div>
         </div>
     </section>
+
+    <section class="banner">
+        <div class="container">
+            <article>
+                <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                <span>Digital Comics</span>
+            </article>
+            <article>
+                <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                <span>Dc Merchandise</span>
+            </article>
+            <article>
+                <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                <span>Subscriptions</span>
+            </article>
+            <article>
+                <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                <span>Comics Shop Locator</span>
+            </article>
+            <article>
+                <img class="visa" src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                <span>DC Power Visa</span>
+            </article>
+        </div>
+    </section>
 @endsection
